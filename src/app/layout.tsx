e6a3cc9447e15fa23d78efb1061425df312bf875
MacBook <macbook@MacBooks-MacBook-Pro.local>
2024-04-21 15:31:33 +0500

    changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,9 @@ import "@uploadthing/react/styles.css";
 import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 import TopNav from "./_components/TopNav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -27,6 +30,15 @@ export default function RootLayout({
       <html lang="en">
         <body className={`font-sans ${inter.variable} flex flex-col gap-4 `}>
           <TopNav />
+          <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
             {children}
         </body>
       </html>
